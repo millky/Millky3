249dc9d862df12f96c08dab5bdb1b9fe22c22f5e
millky <millkyc@gmail.com>
2013-12-24 01:34:18 +0900

    test post

---
+++ src/main/java/com/millky/m3/base/config/WebAppInitializer.java
@@ -0,0 +1,43 @@
+package com.millky.m3.base.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.*;
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * Created by origoni on 2013. 12. 24..
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.millky.m3.base.config");
+        context.setDisplayName("Millky3");
+
+        FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+        servletContext.addListener(new ContextLoaderListener(context));
+        servletContext.setInitParameter("defaultHtmlEscape", "true");
+        servletContext.setInitParameter("spring.profiles.default", "simple");
+        servletContext.setInitParameter("enablePooling", "false");
+
+        context.register(WebMvcConfig.class);
+
+        ServletRegistration.Dynamic appDispatcherServlet = servletContext.addServlet("millkyServlet", new DispatcherServlet(context));
+        appDispatcherServlet.setLoadOnStartup(1);
+        appDispatcherServlet.setAsyncSupported(true);
+
+        Set<String> mappingConflicts = appDispatcherServlet.addMapping("/");
+        if (!mappingConflicts.isEmpty()) {
+            throw new IllegalStateException("'millkyServlet' cannot be mapped to '/'");
+        }
+    }
+}
---
+++ src/main/java/com/millky/m3/base/config/WebMvcConfig.java
@@ -0,0 +1,129 @@
+package com.millky.m3.base.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerMethodArgumentResolver;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by origoni on 2013. 12. 24..
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.millky.m3"})
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    private static final String MESSAGE_SOURCE = "/WEB-INF/i18n/messages";
+
+    @Override
+    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        RequestMappingHandlerMapping requestMappingHandlerMapping = super.requestMappingHandlerMapping();
+        requestMappingHandlerMapping.setUseSuffixPatternMatch(false);
+        requestMappingHandlerMapping.setUseTrailingSlashMatch(false);
+        requestMappingHandlerMapping.setRemoveSemicolonContent(false);
+        return requestMappingHandlerMapping;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource configureMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(MESSAGE_SOURCE);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(5);
+        return messageSource;
+    }
+
+    @Bean(name = "messageSourceAccessor")
+    public MessageSourceAccessor getMessageSourceAccessor() {
+        MessageSourceAccessor messageSource = new MessageSourceAccessor(configureMessageSource());
+        return messageSource;
+    }
+
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("text", "plain", UTF8)));
+        converters.add(stringConverter);
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleResolver getLocaleResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new DeviceResolverHandlerInterceptor());
+        registry.addInterceptor(new SitePreferenceHandlerInterceptor());
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/base/global/footer").setViewName("web/base/globalFooter");
+        registry.addViewController("/base/global/navigation").setViewName("web/base/globalNavigationBar");
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(configureMessageSource());
+        return validator;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/res/**").addResourceLocations("/RESOURCES/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("/RESOURCES/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new DeviceHandlerMethodArgumentResolver());
+        argumentResolvers.add(new SitePreferenceHandlerMethodArgumentResolver());
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setRequestContextAttribute("requestContext");
+        return viewResolver;
+    }
+}
